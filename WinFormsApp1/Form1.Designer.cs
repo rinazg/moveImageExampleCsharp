6698d018dc045052fdda950933b35e625bec65f0
Rina Zviel-Girshin <rinazg@gmail.com>
2021-06-19 17:56:06 +0300

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class frmMovImg
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picB1 = new System.Windows.Forms.PictureBox();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.picB2 = new System.Windows.Forms.PictureBox();
+            this.timerImageMove = new System.Windows.Forms.Timer(this.components);
+            this.btnStopMove = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.lblClickCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picB1
+            // 
+            this.picB1.Image = global::WinFormsApp1.Properties.Resources.hypo;
+            this.picB1.Location = new System.Drawing.Point(1, 104);
+            this.picB1.Name = "picB1";
+            this.picB1.Size = new System.Drawing.Size(156, 179);
+            this.picB1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picB1.TabIndex = 0;
+            this.picB1.TabStop = false;
+            this.picB1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picB1_MouseClick);
+            // 
+            // btnMove
+            // 
+            this.btnMove.BackColor = System.Drawing.Color.Lime;
+            this.btnMove.Font = new System.Drawing.Font("Aharoni", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnMove.Location = new System.Drawing.Point(80, 377);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(211, 38);
+            this.btnMove.TabIndex = 1;
+            this.btnMove.Text = "Click to move image";
+            this.btnMove.UseVisualStyleBackColor = false;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // picB2
+            // 
+            this.picB2.Image = global::WinFormsApp1.Properties.Resources.dog;
+            this.picB2.Location = new System.Drawing.Point(364, 104);
+            this.picB2.Name = "picB2";
+            this.picB2.Size = new System.Drawing.Size(156, 179);
+            this.picB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picB2.TabIndex = 2;
+            this.picB2.TabStop = false;
+            // 
+            // timerImageMove
+            // 
+            this.timerImageMove.Tick += new System.EventHandler(this.timerImageMove_Tick);
+            // 
+            // btnStopMove
+            // 
+            this.btnStopMove.BackColor = System.Drawing.Color.Red;
+            this.btnStopMove.Font = new System.Drawing.Font("Aharoni", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStopMove.Location = new System.Drawing.Point(321, 377);
+            this.btnStopMove.Name = "btnStopMove";
+            this.btnStopMove.Size = new System.Drawing.Size(224, 37);
+            this.btnStopMove.TabIndex = 3;
+            this.btnStopMove.Text = "Stop moving";
+            this.btnStopMove.UseVisualStyleBackColor = false;
+            this.btnStopMove.Click += new System.EventHandler(this.btnStopMove_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Yellow;
+            this.btnRestart.Font = new System.Drawing.Font("Aharoni", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnRestart.Location = new System.Drawing.Point(590, 376);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(178, 37);
+            this.btnRestart.TabIndex = 4;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // lblClickCount
+            // 
+            this.lblClickCount.AutoSize = true;
+            this.lblClickCount.Location = new System.Drawing.Point(281, 9);
+            this.lblClickCount.Name = "lblClickCount";
+            this.lblClickCount.Size = new System.Drawing.Size(164, 25);
+            this.lblClickCount.TabIndex = 5;
+            this.lblClickCount.Text = "number of clicks = ";
+            this.lblClickCount.Visible = false;
+            // 
+            // frmMovImg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblClickCount);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStopMove);
+            this.Controls.Add(this.picB2);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.picB1);
+            this.Name = "frmMovImg";
+            this.Text = "Moving image example";
+            ((System.ComponentModel.ISupportInitialize)(this.picB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picB2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picB1;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.PictureBox picB2;
+        private System.Windows.Forms.Timer timerImageMove;
+        private System.Windows.Forms.Button btnStopMove;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label lblClickCount;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WinFormsApp1
+{
+    public partial class frmMovImg : Form
+    {
+        bool collision;
+        string[] imageArr;  //reading all file names from 
+        int counter;//counts number of clicks on moving image
+
+
+        public frmMovImg()
+        {
+            InitializeComponent();
+            createRandomImageArray();
+            changeImage(picB2);
+            changeImage(picB1);
+        }
+
+        
+        //read file names from Resources directory in the project
+        //to array and create a random order 
+        private void createRandomImageArray()
+        {
+            //read file names from Resources directory in the project
+            imageArr = Directory.GetFiles(@"../../../Resources/");
+            shuffle(imageArr);
+        }
+
+        // basic Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        private void  shuffle(string[] arr)
+        {
+            Random rnd = new Random();
+            for (int t = 0; t < arr.Length; t++)
+            {
+                string tmp = arr[t];
+                int r = rnd.Next(arr.Length);
+                arr[t] = arr[r];  //swap 2 elements
+                arr[r] = tmp;
+            }
+        }
+        private void timerImageMove_Tick(object sender, EventArgs e)
+        {
+            if (picB1.Location.X + picB1.Width <= picB2.Location.X && collision==false )
+                picB1.Location = new Point(picB1.Location.X + 10, picB1.Location.Y);
+            else //collision happened
+                if (picB1.Location.X + picB1.Width > picB2.Location.X && collision == false)
+                {   collision = true;
+                //change image
+                changeImage(picB2);
+                }
+               //moving before reaching the Width of Form
+               else if(picB2.Location.X + picB2.Width <= this.Width-10) //stop at the end of form
+                picB2.Location = new Point(picB2.Location.X + 10, picB2.Location.Y);
+        }
+
+        private void changeImage(PictureBox p)
+        {
+            Random rnd = new Random();
+            int r = rnd.Next(imageArr.Length);
+            p.ImageLocation = imageArr[r];
+        }
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            timerImageMove.Start();
+        }
+
+        private void btnStopMove_Click(object sender, EventArgs e)
+        {
+            timerImageMove.Stop();
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            collision = false;
+            picB1.Location = new System.Drawing.Point(1, 104);
+            picB2.Location = new System.Drawing.Point(364, 104);
+            changeImage(picB2);
+            changeImage(picB1);
+        }
+
+        private void picB1_MouseClick(object sender, MouseEventArgs e)
+        {
+            //increase and show click counter only if image is moving
+            if (timerImageMove.Enabled == true && collision==false)
+            {
+                //show image name and click counter
+                lblClickCount.Visible = true;
+                //image location
+                string name = ((PictureBox)sender).ImageLocation.ToString();
+                char[] delimiterChars = { '.' , '/'};
+                string[] arr = name.Split(delimiterChars);
+                //only image name
+                name = arr[arr.Length - 2];
+                lblClickCount.Text = "number of clicks on moving " + name + ++counter;
+            }
+           
+        }
+    }
+}
